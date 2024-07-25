32f2aa56d237a8e16df89085d5e16b147e791a72
ic5594 <ic5594@naver.com>
2024-07-25 11:41:37 +0900

    파일 이동에 따른 커밋

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
+import './assets/css/index.css';
 import store from './store/index.js';
 import { Provider } from 'react-redux';
 
