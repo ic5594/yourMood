07fe92e52ab3677229655762a49052923e885c68
ic5594 <ic5594@naver.com>
2024-07-25 16:23:25 +0900

    매뉴변경 (장바구니 -> 카카오톡 채널)

--- src/store/Menu.js
+++ src/store/Menu.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  menu: ['카페소개', '메뉴', '오시는 길', '인스타그램', '장바구니'],
+  menu: ['카페소개', '메뉴', '오시는 길', '인스타그램', '카카오톡 채널'],
   path: ['/introduction', '/menu', '/waymap']
 }
 
