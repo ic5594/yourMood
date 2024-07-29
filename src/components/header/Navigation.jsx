f692019c30c6956bd18f7ea6f62869bd5592a5e8
ic5594 <ic5594@naver.com>
2024-07-29 14:38:14 +0900

    네비게이션 바 추가

--- src/components/header/Navigation.jsx
+++ src/components/header/Navigation.jsx
@@ -4,11 +4,12 @@ import { motion } from 'framer-motion';
 
 const Navigation = ({ index, children }) => {
   const path = useSelector((state) => state.menu.path); //redux path
-  const menu = useSelector((state) => state.menu.menu); //redux menu
 
   const setMenu = index < 3;
   const yourmoodInstaUrl =
     'https://www.instagram.com/cafeyourmood/p/Ct23jDKvaV9/?img_index=1';
+
+  const yourmoodKakaoCh = 'https://pf.kakao.com/_DnRlxj';
 
   const initMotion = {
     whileHover: { scale: 1.1 },
@@ -17,15 +18,6 @@ const Navigation = ({ index, children }) => {
 
   return (
     <>
-      <nav>
-        <ul>
-          {menu.map((item, index) => (
-            <Navigation key={item} index={index} selected>
-              {item}
-            </Navigation>
-          ))}
-        </ul>
-      </nav>
       {setMenu && (
         <Link to={path[index]}>
           <motion.li
@@ -39,14 +31,18 @@ const Navigation = ({ index, children }) => {
         </Link>
       )}
       {!setMenu && (
-        <motion.li
-          className="menu"
-          whileHover={initMotion.whileHover}
-          transition={initMotion.transition}
-          onClick={() => window.open(yourmoodInstaUrl)}
-        >
-          {children}
-        </motion.li>
+        <a href="javascript: void(0)">
+          <motion.li
+            className="menu"
+            whileHover={initMotion.whileHover}
+            transition={initMotion.transition}
+            onClick={() =>
+              window.open(index === 2 ? yourmoodInstaUrl : yourmoodKakaoCh)
+            }
+          >
+            {children}
+          </motion.li>
+        </a>
       )}
     </>
   );
