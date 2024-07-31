4228adb5f27c3066d16453fc47025f5f0544b8af
ic5594 <ic5594@naver.com>
2024-07-31 14:20:55 +0900

    카카오 맵

--- src/pages/waymap/WayMap.jsx
+++ src/pages/waymap/WayMap.jsx
@@ -1,5 +1,24 @@
+import { Map, MapMarker, CustomOverlayMap } from 'react-kakao-maps-sdk';
+import { useState } from 'react';
 const WayMap = () => {
-  return <p> waymap </p>;
+  const [level, setLevel] = useState(3);
+
+  const mapData = { lat: 37.572712, lng: 126.807901 };
+
+  return (
+    <Map
+      center={mapData}
+      style={{ width: '400px', height: '300px', borderRadius: '5px' }}
+      level={level}
+    >
+      <MapMarker position={mapData} style={{ border: 'tranparent' }}>
+        <div className="name">Yourmood</div>
+      </MapMarker>
+
+      <button onClick={() => setLevel(level + 1)}>-</button>
+      <button onClick={() => setLevel(level - 1)}>+</button>
+    </Map>
+  );
 };
 
 export default WayMap;
