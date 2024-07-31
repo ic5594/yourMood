dbf0177841a2c49d0e0d6adf985b84dac7fe7f04
ic5594 <ic5594@naver.com>
2024-07-31 18:22:24 +0900

    메뉴바 실행시 스크롤 바 삭제

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,26 +1,47 @@
 import { createPortal } from 'react-dom';
 import { motion } from 'framer-motion';
+import { useEffect } from 'react';
 
 import { IoClose } from 'react-icons/io5';
 
 export default function Modal({ children, onClose }) {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    document.body.style.height = '100%';
+
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [onClose]);
+
   return createPortal(
     <>
-      <IoClose size={40} />
       <div className="backdrop" onClick={onClose} />
-      <motion.dialog
-        variants={{
-          hidden: { opacity: 0, x: 30 },
-          visible: { opacity: 1, x: 0 },
-        }}
-        open
-        className="modal"
-        initial="hidden"
-        animate="visible"
-        exit={{ opacity: 0, x: 30 }}
-      >
-        {children}
-      </motion.dialog>
+      <div className="modal_area">
+        <IoClose
+          size={37}
+          style={{
+            zIndex: '10',
+            color: 'white',
+            position: 'absolute',
+            left: '30%',
+          }}
+          onClick={onClose}
+        />
+        <motion.dialog
+          variants={{
+            hidden: { opacity: 0, x: 30 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          open
+          className="modal"
+          initial="hidden"
+          animate="visible"
+          exit={{ opacity: 0, x: 30 }}
+        >
+          {children}
+        </motion.dialog>
+      </div>
     </>,
     document.getElementById('navbar')
   );
